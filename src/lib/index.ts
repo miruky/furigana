74cc9920d20d03522c06e044ae09a909ef3834a8
miruky <h.miyoki@gmail.com>
2026-04-22 17:25:19 +0900

    ローマ字APIを公開エクスポートに追加

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,15 @@
 export { alignReading } from './align';
 export type { RubySegment } from './align';
-export { annotateTokens, toBracketText, toHiraganaText, toRubyHtml, toWakachi } from './annotate';
+export {
+  annotateTokens,
+  toBracketText,
+  toHiraganaText,
+  toRomajiText,
+  toRubyHtml,
+  toWakachi,
+} from './annotate';
 export type { AnnotatedToken, TokenReading } from './annotate';
 export { hasKanji, isKanji, katakanaToHiragana } from './kana';
+export { hiraganaToRomaji } from './romaji';
 export { loadTokenizer, readTokenLines } from './tokenizer';
 export type { JaTokenizer } from './tokenizer';
